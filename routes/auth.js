8d8be3421fc1d5c4c5dbaa0d482687b84cb44ec8
Jace254 <joashmacer@gmail.com>
2022-05-30 00:13:57 +0300

    check for user duplicates and encrypted passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,35 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const Joi = require('@hapi/joi');
-
-const validationSchema = Joi.object({
-    name: Joi.string().min(2).max(255).required(),
-    email: Joi.string().min(6).max(255).required().email(),
-    password: Joi.string().min(8).max(1024).required()
-});
+const {DataValidation} = require('../Validation/DataValidation');
 
 
 router.post('/register', async (req,res) => {
-    const {error} = validationSchema.validate(req.body);
-
+    //Validate body according to our requirements
+    const {error} = DataValidation.registerValiation(req.body);
     if(error) return res.status(401).send(error);
-    else{
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
-    
-        try{
-            const savedUser = await user.save();
-            res.status(201).send(savedUser);
-        }
-        catch(e){
-            res.status(401).send(e);
-        }
+
+    //check for user duplicates
+    const userEmailExists = await User.findOne({email: req.body.email});
+    if(userEmailExists) return res.status(409).send("A user with that email already exists.");
+
+    //encrypt pass
+    const encryptedPass = bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: encryptedPass
+    });
+
+    try{
+        const savedUser = await user.save();
+        res.status(201).send(savedUser);
     }
+    catch(e){
+        res.status(401).send(e);
+    }
+    
 
 });
 router.delete('/register/:id',  (req,res) => {
