fcfd6c483cbacd7590e87b6925b10a40a1778ed5
Jace254 <joashmacer@gmail.com>
2022-05-29 23:06:09 +0300

    added delete router,added a validation schema and made some tests

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,39 @@
 const router = require('express').Router();
+const User = require('../models/user');
+const Joi = require('@hapi/joi');
+
+const validationSchema = Joi.object({
+    name: Joi.string().min(2).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(8).max(1024).required()
+});
 
 
-router.post('/register', (req,res) => {
-    res.send('Register');
+router.post('/register', async (req,res) => {
+    const {error} = validationSchema.validate(req.body);
+
+    if(error) return res.status(401).send(error);
+    else{
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+    
+        try{
+            const savedUser = await user.save();
+            res.status(201).send(savedUser);
+        }
+        catch(e){
+            res.status(401).send(e);
+        }
+    }
+
+});
+router.delete('/register/:id',  (req,res) => {
+    User.findByIdAndRemove({_id: req.params.id}).then(async (user) =>{
+        await res.send(user);
+    });
 });
 
 module.exports = router;
