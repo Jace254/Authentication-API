90a25e3cf100fa63e49cb66e847564930779a496
Jace254 <joashmacer@gmail.com>
2022-05-29 23:09:38 +0300

    added body parser and connected to database

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 
 const app = express();
 
-app.use('/api/user',authRoute, () =>{
+//body-parser middleware
+app.use(express.json());
 
-});
+//route middleware
+app.use('/api/user',authRoute);
 
 app.listen(process.env.PORT || 4000, () =>{
     console.log('App running on port 4000');
 });
+
+mongoose.connect(process.env.DB_URI, () =>{
+    console.log('Connected to DB')
+})
