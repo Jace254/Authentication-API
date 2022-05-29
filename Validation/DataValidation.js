e0b97a278e87721d5c3f7fe332cf6c771d037727
Jace254 <joashmacer@gmail.com>
2022-05-29 23:30:18 +0300

    extracted user,email,password validation

---
+++ Validation/DataValidation.js
@@ -0,0 +1,15 @@
+const Joi = require('@hapi/joi');
+
+class DataValidation{
+    static registerValiation(request){
+        const validationSchema = Joi.object({
+            name: Joi.string().min(2).max(255).required(),
+            email: Joi.string().min(6).max(255).required().email(),
+            password: Joi.string().min(8).max(1024).required()
+        });
+    
+        return validationSchema.validate(request);
+    }
+}
+
+module.exports = {DataValidation};
